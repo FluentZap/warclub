49d63c5bcec59ae4747872b2b7a69f6d74ec7554
Zaldren <Zaldren@gmail.com>
2022-10-15 17:37:45 -0700

    add compendium split units class

--- model/Units.cs
+++ model/Units.cs
@@ -1,107 +1,27 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 
-namespace WarClub
+namespace WarClub;
+
+class Models
 {
-  class Models
-  {
-    public int Id;
-    public string Name;
-    public string Image;
-    public int Count;
-    public Point Size = new Point();
-    public HashSet<string> Types = new HashSet<string>();
-  }
+  public int Id;
+  public string Name;
+  public string Image;
+  public int Count;
+  public Point Size = new Point();
+  public HashSet<string> Types = new HashSet<string>();
+}
 
-  class DataSheet
-  {
-    public int Id;
-    public string Name;
-    public string FactionId;
-    // Troops
-    // Elites
-    // Heavy Support
-    // HQ
-    // Fast Attack
-    // Dedicated Transport
-    // Flyers
-    // Lords of War
-    // Fortifications
-    public string Role;
-    public string UnitComposition;
-    public Dictionary<string, UnitStats> Units = new Dictionary<string, UnitStats>();
-    public List<Wargear> Wargear = new List<Wargear>();
-    public List<Wargear> DefaultWargear = new List<Wargear>();
-    public List<string> WargearOptions = new List<string>();
-    public List<string> FactionKeywords = new List<string>();
-    public List<string> Keywords = new List<string>();
-    public List<Stratagem> Stratagems = new List<Stratagem>();
-  }
+class Unit
+{
+  public DataSheet DataSheet;
+  public Dictionary<string, UnitLine> UnitLines = new Dictionary<string, UnitLine>();
+}
 
-  class UnitStats
-  {
-    public string Movement;
-    public string WS;
-    public string BS;
-    public string S;
-    public string T;
-    public string W;
-    public string A;
-    public string Ld;
-    public string Sv;
-    public int Cost;
-    public int MinModelsPerUnit;
-    public int MaxModelsPerUnit;
-    public Point Size = new Point();
-  }
-
-  class Wargear
-  {
-    public int Id;
-    public string Name;
-    // Melee/Ranged/Other
-    public string Archetype;
-    public string Description;
-    public bool Relic;
-    public string FactionId;
-    public string Legend;
-    public int Cost;
-
-    public Dictionary<string, WargearLine> WargearLine = new Dictionary<string, WargearLine>();
-
-  }
-
-  class WargearLine
-  {
-    public string Name;
-    public string Range;
-    public string Type;
-    public string S;
-    public string AP;
-    public string D;
-    public string Abilities;
-  }
-
-  class Unit
-  {
-    public DataSheet DataSheet;
-    public Dictionary<string, UnitLine> UnitLines = new Dictionary<string, UnitLine>();
-  }
-
-  class UnitLine
-  {
-    public UnitStats UnitStats;
-    public int Count;
-    public List<Wargear> Wargear = new List<Wargear>();
-  }
-
-  class Stratagem
-  {
-    public int Id;
-    public string Name;
-    public string Type;
-    public string Cost;
-    public string Legend;
-    public string Description;
-  }
+class UnitLine
+{
+  public UnitStats UnitStats;
+  public int Count;
+  public List<Wargear> Wargear = new List<Wargear>();
 }
